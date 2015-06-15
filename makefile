d996a739a0ad3aca61c9d7a642339a2fce7256a1
bcumming <bcumming@cscs.ch>
2015-06-15 17:11:00 +0200

    clean up utils and makefile

--- makefile
+++ makefile
@@ -4,60 +4,63 @@ FLAGS+=-O3
 
 LINK=-lcublas -lcuda
 
-all : axpy_cublas axpy_kernel memcopy memcopy2 memcopy3 blur diffusion2d diffusion2d_mpi blur_twice blur_twice.omp dot
+CUDA_BENCHMARKS= axpy_cublas.cuda axpy_kernel.cuda memcopy.cuda memcopy2.cuda memcopy3.cuda blur.cuda diffusion2d.cuda diffusion2d_mpi.cuda blur_twice.cuda dot.cuda
+DIFFUSION_BENCHMARKS= diffusion2d.cuda diffusion2d_mpi.cuda
+OPENMP_BENCHMARKS= blur_twice.omp axpy.omp
 
-axpy_cublas : axpy_cublas.cu util.h
-	nvcc $(FLAGS) axpy_cublas.cu -o axpy_cublas $(LINK)
+BENCHMARKS=$(CUDA_BENCHMARKS)
 
-axpy_kernel : axpy_kernel.cu util.h
-	nvcc $(FLAGS) axpy_kernel.cu -o axpy_kernel $(LINK)
+all : $(BENCHMARKS)
 
-axpy_omp : axpy_omp.cpp
-	g++ -std=c++11 -fopenmp -O3 axpy_omp.cpp -o axpy_omp
+#----------------------------------------
+# CUDA benchmarks
+#----------------------------------------
+axpy_cublas.cuda : axpy_cublas.cu util.h
+	nvcc $(FLAGS) axpy_cublas.cu -o axpy_cublas.cuda $(LINK)
 
-memcopy : memcopy.cu util.h
-	nvcc $(FLAGS) memcopy.cu -o memcopy $(LINK)
+axpy_kernel.cuda : axpy_kernel.cu util.h
+	nvcc $(FLAGS) axpy_kernel.cu -o axpy_kernel.cuda $(LINK)
 
-memcopy2 : memcopy2.cu util.h
-	nvcc $(FLAGS) memcopy2.cu -o memcopy2 $(LINK)
+memcopy.cuda : memcopy.cu util.h
+	nvcc $(FLAGS) memcopy.cu -o memcopy.cuda $(LINK)
 
-memcopy3 : memcopy3.cu util.h
-	nvcc $(FLAGS) memcopy3.cu -o memcopy3 $(LINK)
+memcopy2.cuda : memcopy2.cu util.h
+	nvcc $(FLAGS) memcopy2.cu -o memcopy2.cuda $(LINK)
 
-blur : blur.cu util.h
-	nvcc $(FLAGS) blur.cu -o blur $(LINK)
+memcopy3.cuda : memcopy3.cu util.h
+	nvcc $(FLAGS) memcopy3.cu -o memcopy3.cuda $(LINK)
 
-diffusion2d : diffusion2d.cu util.h diffusion_kernels.o
+blur.cuda : blur.cu util.h
+	nvcc $(FLAGS) blur.cu -o blur.cuda $(LINK)
+
+blur_twice.cuda : blur_twice.cu util.h
+	nvcc $(FLAGS) blur_twice.cu -o blur_twice.cuda $(LINK)
+
+dot.cuda : dot.cu util.h
+	nvcc $(FLAGS) dot.cu -o dot.cuda $(LINK)
+
+#----------------------------------------
+# diffusion2D benchmarks
+#----------------------------------------
+diffusion2d.cuda : diffusion2d.cu util.h diffusion_kernels.o
 	nvcc -c $(FLAGS) diffusion2d.cu $(LINK) -I$(CRAY_MPICH2_DIR)/include
-	CC diffusion2d.o diffusion_kernels.o -o diffusion2d
+	CC diffusion2d.o diffusion_kernels.o -o diffusion2d.cuda
 
 diffusion_kernels.o : diffusion_kernels.cu
 	nvcc -c $(FLAGS) diffusion_kernels.cu $(LINK) -I$(CRAY_MPICH2_DIR)/include
 
-diffusion2d_mpi : diffusion2d_mpi.cpp util.h diffusion_kernels.o
-	CC -std=c++11 -O3 diffusion2d_mpi.cpp diffusion_kernels.o -o diffusion2d_mpi
+diffusion2d_mpi.cuda : diffusion2d_mpi.cpp util.h diffusion_kernels.o
+	CC -std=c++11 -O3 diffusion2d_mpi.cpp diffusion_kernels.o -o diffusion2d_mpi.cuda
 
-blur_twice : blur_twice.cu util.h
-	nvcc $(FLAGS) blur_twice.cu -o blur_twice $(LINK)
-
+#----------------------------------------
+# openmp benchmarks
+#----------------------------------------
 blur_twice.omp : blur_twice.cpp util.h
 	CC -std=c++11 -O3 -fopenmp blur_twice.cpp -o blur_twice.omp $(LINK)
 
-dot : dot.cu util.h
-	nvcc $(FLAGS) dot.cu -o dot $(LINK)
-
-test : axpy_cublas axpy_kernel
-	aprun axpy_cublas 20
-	aprun axpy_kernel 20
+axpy.omp : axpy_omp.cpp
+	g++ -std=c++11 -fopenmp -O3 axpy_omp.cpp -o axpy.omp
 
 clean :
-	rm -f axpy_cublas
-	rm -f axpy_kernel
-	rm -f axpy_omp
-	rm -f memcopy
-	rm -f memcopy2
-	rm -f memcopy3
-	rm -f blur
-	rm -f diffusion2d
-	rm -f diffusion2d_mpi
-	rm -f blur_twice
+	rm -f *.cuda
+	rm -f *.omp
--- util.h
+++ util.h
@@ -6,9 +6,8 @@
 #ifndef NO_CUDA
 #include <cublas_v2.h>
 
-///////////////////////////////////////////////////////////////////////////////
-// cublas
-///////////////////////////////////////////////////////////////////////////////
+// helper for initializing cublas
+// use only for demos: not threadsafe
 static cublasHandle_t get_cublas_handle() {
     static bool is_initialized = false;
     static cublasHandle_t cublas_handle;
@@ -20,7 +19,7 @@ static cublasHandle_t get_cublas_handle() {
 }
 
 ///////////////////////////////////////////////////////////////////////////////
-// error checking
+// CUDA error checking
 ///////////////////////////////////////////////////////////////////////////////
 static void cuda_check_status(cudaError_t status) {
     if(status != cudaSuccess) {
@@ -42,6 +41,8 @@ static void cuda_check_last_kernel(std::string const& errstr) {
 ///////////////////////////////////////////////////////////////////////////////
 // allocating memory
 ///////////////////////////////////////////////////////////////////////////////
+
+// allocate space on GPU for n instances of type T
 template <typename T>
 T* malloc_device(size_t n) {
     void* p;
@@ -50,6 +51,7 @@ T* malloc_device(size_t n) {
     return (T*)p;
 }
 
+// allocate page-locked host memory for n instances of type T
 template <typename T>
 T* malloc_host_pinned(size_t N, T value=T()) {
     T* ptr = nullptr;
@@ -63,6 +65,11 @@ T* malloc_host_pinned(size_t N, T value=T()) {
 ///////////////////////////////////////////////////////////////////////////////
 // copying memory
 ///////////////////////////////////////////////////////////////////////////////
+
+// copy n*T from host to device
+// If a cuda stream is passed as the final argument the copy will be performed
+// asynchronously in the specified stream, otherwise it will be serialized in
+// the default (NULL) stream
 template <typename T>
 void copy_to_device_async(const T* from, T* to, size_t n, cudaStream_t stream=NULL) {
     auto status =
@@ -70,6 +77,10 @@ void copy_to_device_async(const T* from, T* to, size_t n, cudaStream_t stream=NU
     cuda_check_status(status);
 }
 
+// copy n*T from device to host
+// If a cuda stream is passed as the final argument the copy will be performed
+// asynchronously in the specified stream, otherwise it will be serialized in
+// the default (NULL) stream
 template <typename T>
 void copy_to_host_async(const T* from, T* to, size_t n, cudaStream_t stream=NULL) {
     auto status =
@@ -77,6 +88,7 @@ void copy_to_host_async(const T* from, T* to, size_t n, cudaStream_t stream=NULL
     cuda_check_status(status);
 }
 
+// copy n*T from host to device
 template <typename T>
 void copy_to_device(T* from, T* to, size_t n) {
     auto status =
@@ -84,6 +96,7 @@ void copy_to_device(T* from, T* to, size_t n) {
     cuda_check_status(status);
 }
 
+// copy n*T from device to host
 template <typename T>
 void copy_to_host(T* from, T* to, size_t n) {
     auto status =
@@ -92,10 +105,11 @@ void copy_to_host(T* from, T* to, size_t n) {
 }
 
 #endif
+///////////////////////////////////////////////////////////////////////////////
+// everything below works both with gcc and nvcc
+///////////////////////////////////////////////////////////////////////////////
 
-///////////////////////////////////////////////////////////////////////////////
-// command line arguments
-///////////////////////////////////////////////////////////////////////////////
+// read command line arguments
 static size_t read_arg(int argc, char** argv, size_t index, int default_value) {
     if(argc>index) {
         try {
@@ -123,12 +137,12 @@ T* malloc_host(size_t N, T value=T()) {
     return ptr;
 }
 
-///////////////////////////////////////////////////////////////////////////////
-// timing functions
-///////////////////////////////////////////////////////////////////////////////
+// aliases for types used in timing host code
 using clock_type    = std::chrono::high_resolution_clock;
 using duration_type = std::chrono::duration<double>;
 
+// return the time in seconds since the get_time function was first called
+// for demos only: not threadsafe
 static double get_time() {
     static auto start_time = clock_type::now();
     return duration_type(clock_type::now()-start_time).count();
